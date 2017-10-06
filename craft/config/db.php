65ed116734075931a4dbd63b76be72958a290fe2
James Greig <3stripe@gmail.com>
2017-10-06 14:28:40 +0100

    /public is now /html, and /storage ignored

--- craft/config/db.php
+++ craft/config/db.php
@@ -7,21 +7,29 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
  */
 
-return array(
+ return array(
+    '*' => array(
+        'omitScriptNameInUrls' => true,
+    ),
 
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
+    'craftcms' => array(
+			// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+			'server' => 'localhost',
 
-	// The name of the database to select.
-	'database' => 'craftcms',
+			// The name of the database to select.
+			'database' => 'craftcms',
 
-	// The database username to connect with.
-	'user' => 'root',
+			// The database username to connect with.
+			'user' => 'root',
 
-	// The database password to connect with.
-	'password' => 'mysql',
+			// The database password to connect with.
+			'password' => 'mysql',
 
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
+			// The prefix to use when naming tables. This can be no more than 5 characters.
+			'tablePrefix' => 'craft',
+    ),
 
+    'digitalocean' => array(
+        'cooldownDuration' => 0,
+    )
 );
--- craft/config/general.php
+++ craft/config/general.php
@@ -7,30 +7,39 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
-return array(
+ return array(
+    '*' => array(
+        'omitScriptNameInUrls' => true,
+    ),
 
-	// Base site URL
-	'siteUrl' => '',
+    'craftcms' => array(
+			// Base site URL
+			'siteUrl' => '',
 
-	// Environment-specific variables (see https://craftcms.com/docs/multi-environment-configs#environment-specific-variables)
-	'environmentVariables' => array(),
+			// Environment-specific variables (see https://craftcms.com/docs/multi-environment-configs#environment-specific-variables)
+			'environmentVariables' => array(),
 
-	// Default Week Start Day (0 = Sunday, 1 = Monday...)
-	'defaultWeekStartDay' => 0,
+			// Default Week Start Day (0 = Sunday, 1 = Monday...)
+			'defaultWeekStartDay' => 0,
 
-	// Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
-	'enableCsrfProtection' => true,
+			// Enable CSRF Protection (recommended, will be enabled by default in Craft 3)
+			'enableCsrfProtection' => true,
 
-	// Whether "index.php" should be visible in URLs (true, false, "auto")
-	'omitScriptNameInUrls' => 'true',
+			// Whether "index.php" should be visible in URLs (true, false, "auto")
+			'omitScriptNameInUrls' => 'true',
 
-	// Control Panel trigger word
-	'cpTrigger' => 'admin',
+			// Control Panel trigger word
+			'cpTrigger' => 'admin',
 
-	// Dev Mode (see https://craftcms.com/support/dev-mode)
-	'devMode' => false,
+			// Dev Mode (see https://craftcms.com/support/dev-mode)
+			'devMode' => false,
 
-	// changed to ensure PayPal checkout works
-	'tokenParam' => 'craftToken',
+			// changed to ensure PayPal checkout works
+			'tokenParam' => 'craftToken',
 
+    ),
+
+    'digitalocean' => array(
+			// TODO;
+    )
 );
